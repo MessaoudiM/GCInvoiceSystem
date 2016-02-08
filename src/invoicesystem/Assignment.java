41187c3db4e85fd13a470ebed7511913d73f41e3
MessaoudiMo <mo.messaoudi.rotterdam@gmail.com>
2016-02-08 17:18:39 +0100

    Changes to Assignment.java Added 6 fields, constructor, getters & setters for: contact rate startDateTime endDateTime hoursMappedToRate shift

--- src/invoicesystem/Assignment.java
+++ src/invoicesystem/Assignment.java
@@ -5,10 +5,82 @@
  */
 package invoicesystem;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.HashMap;
+
 /**
  *
  * @author hassanmessaoudi
  */
 public class Assignment {
+    
+    private Contact contact;
+    private float rate;
+    private LocalDateTime startDateTime, endDateTime;
+    private Map<Float, Map<LocalDateTime, LocalDateTime>> hoursMappedToRate;
+    private String shift;
+    
+    public Assignment(Contact contact, float rate, LocalDateTime startDateTime, 
+                      LocalDateTime endTime, String shift){
+        
+        this.contact = contact;
+        this.rate = rate;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+        this.shift = shift;
+    
+    }
+
+    
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(float rate) {
+        this.rate = rate;
+    }
+
+    public LocalDateTime getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(LocalDateTime startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public LocalDateTime getEndDateTime() {
+        return endDateTime;
+    }
+
+    public void setEndDateTime(LocalDateTime endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+    public Map<Float, Map<LocalDateTime, LocalDateTime>> getHoursMappedToRate() {
+        return hoursMappedToRate;
+    }
+
+    public void setHoursMappedToRate(Map<Float, Map<LocalDateTime, LocalDateTime>> hoursMappedToRate) {
+        this.hoursMappedToRate = hoursMappedToRate;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+    
+   
     
 }
